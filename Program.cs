7fad1b2d3672c60beea871c9b967e6ddc96eb8dd
Igor Korzhan <igor.human@gmail.com>
2024-03-28 23:46:55 +0200

    Зробив завдання №1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Drawing;
+using System.Text;
+
+// Домашнє завдання №1
+// Завдання 2
+
+namespace Classes
+{
+    class Program
+    {
+        class Rectangle
+        {
+            private double side1, side2;
+
+            public double AreaCalculator() //метод обчислює площу
+            {
+                return side1 * side2;
+            }
+
+            public double PerimeterCalculator() //метод обчислює периметр
+            {
+                return (side1 + side2) * 2;
+            }
+
+            public double Area //тільки приймаємо властивість площа
+            {
+                get { return AreaCalculator(); }
+            }
+
+            public double Perimeter //тільки приймаємо властивіть периметр
+            {
+                get { return PerimeterCalculator(); }
+            }
+
+            public Rectangle(double side1, double side2) //Конструктор приймає 2 параметри
+            {
+                this.side1 = side1;
+                this.side2 = side2;
+            }
+        }
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введіть довжину першої сторони прямокутника:");
+            double.TryParse(Console.ReadLine(), out double side1);
+            Console.WriteLine("Введіть довжину другої сторони прямокутника:");
+            double.TryParse(Console.ReadLine(), out double side2);
+
+            Rectangle rect = new Rectangle(side1, side2);
+            Console.WriteLine("Периметр: " +  rect.Perimeter);
+            Console.WriteLine("Площа: " + rect.Area);
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
